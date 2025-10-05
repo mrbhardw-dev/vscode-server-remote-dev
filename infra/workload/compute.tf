999e5930fc75881fb851283f77f009b8676335d1
Mritunjay Bhardwaj <mritunjay.bhardwaj@outlook.com>
2025-10-05 17:36:57 +0000

    TFsec

--- infra/workload/compute.tf
+++ infra/workload/compute.tf
@@ -66,7 +66,14 @@ resource "google_compute_instance" "vscode_server" {
 
   service_account {
     email  = google_service_account.instance_sa.email
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
   }
 
   allow_stopping_for_update = true
